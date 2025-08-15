f6fb15f2068336386f07ddabd01c6b2f3a55e833
unknown <shinde.chaitanya13@gmail.com>
2025-08-15 17:04:13 +0530

    button && overflow-scroll

--- src/components/ButtonList.jsx
+++ src/components/ButtonList.jsx
@@ -1,7 +1,24 @@
 import React from "react";
-
+import Button from "./Button";
+const dummyButtonList = [
+  "All",
+  "Trending",
+  "Gaming",
+  "Songs",
+  "Live",
+  "Soccer",
+  "Cricket",
+  "Cooking",
+  "News",
+];
 const ButtonList = () => {
-  return <div>ButtonList</div>;
+  return (
+    <div className="w-full flex overflow-x-scroll no-scrollbar">
+      {dummyButtonList.map((b) => (
+        <Button name={b} />
+      ))}
+    </div>
+  );
 };
 
 export default ButtonList;
--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,7 +4,7 @@ import VideoContainer from "./VideoContainer";
 
 const MainContainer = () => {
   return (
-    <div className="flex-1 p-4 shadow-lg rounded-lg">
+    <div className="w-[calc(100%-136px)] flex-1 p-4 shadow-lg rounded-lg">
       <ButtonList />
       <VideoContainer />
     </div>
